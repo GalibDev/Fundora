bb38ab63137a729adb4629d0d1788b01485afe55
GalibDev <galibdev@users.noreply.github.com>
2026-07-23 18:41:45 +0600

    feat(client): add login route

---
+++ client/app/login/page.tsx
@@ -0,0 +1,2 @@
+import Navbar from "@/components/navbar";import AuthForm from "@/components/auth-form";
+export default function Login(){return <><Navbar/><main className="container-app grid min-h-[calc(100vh-80px)] place-items-center py-12"><AuthForm/></main></>}
